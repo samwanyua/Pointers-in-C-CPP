7a1d9a05349228782de7954e12b836c1d035b3f0
samwanyua <samexample8@gmail.com>
2024-03-21 23:26:12 +0300

    Implementing pointer arithmetics

--- intro.cpp
+++ intro.cpp
@@ -6,6 +6,19 @@ int main(){
 int a = 90;
 int *p;
 p = &a;
+*p =13;
+
+double gpa = 2.56;
+double* pGpa = &gpa; //Pointer to gpa
+
+cout << *pGpa << endl; //2.56
+
+// pointer arithmetic
+cout << *pGpa  + 1<< endl; //3.56
+cout << *(pGpa  + 1)<< endl; //garbage value
+cout << sizeof(int) << endl; //4 bytes
+cout << pGpa  + 1<< endl; //3.56
+
 
 cout << p << endl; //prints the address
 cout << &a << endl; //prints the address
